5851d0c87bdcdfdc54b5b7928b3a8662892bb769
sueselle <sue@thehappyuser.com>
2023-11-28 03:23:47 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var shoutIt = function (name) {
+  var shout = `Hello, ${name}, nice to meet you!`.toUpperCase();
+  return shout;
+};
+console.log(shoutIt("Sue"));
+console.log(shoutIt("Buster"));
+console.log(shoutIt("Juno"));
